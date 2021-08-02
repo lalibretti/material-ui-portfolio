9b8640e6ac99e1f7927f4c396bf05c0e61821426
Lauren Libretti <lalibretti@yahoo.com>
2021-08-01 23:00:45 -0400

    Updates

---
+++ precache-manifest.407695d0f5881ca042f7e4e27923566f.js
@@ -0,0 +1,46 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "1520a2659aff4f5944ea835fa62915b4",
+    "url": "/portfolio/index.html"
+  },
+  {
+    "revision": "f56087430d411c9ef732",
+    "url": "/portfolio/static/css/main.62d5269a.chunk.css"
+  },
+  {
+    "revision": "8f9dd49be73edccdf312",
+    "url": "/portfolio/static/js/2.64c78da3.chunk.js"
+  },
+  {
+    "revision": "84f48c9a090ba2f8cc4a11e48dd3dad0",
+    "url": "/portfolio/static/js/2.64c78da3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "f56087430d411c9ef732",
+    "url": "/portfolio/static/js/main.65308d01.chunk.js"
+  },
+  {
+    "revision": "7a6cc99bd8f08f9fa6ec",
+    "url": "/portfolio/static/js/runtime-main.209c2d7e.js"
+  },
+  {
+    "revision": "92645cddb034ebba7759d298f6e02b3d",
+    "url": "/portfolio/static/media/avatar.92645cdd.png"
+  },
+  {
+    "revision": "2dd213aa54b22c3bca0c62f570d1512a",
+    "url": "/portfolio/static/media/coffee-apple.2dd213aa.jpg"
+  },
+  {
+    "revision": "54fd423489a1937caf8950d12c8a8526",
+    "url": "/portfolio/static/media/html-css-javascript-lg.54fd4234.jpg"
+  },
+  {
+    "revision": "a8b012a0d7afd08ee07251e1f67294ff",
+    "url": "/portfolio/static/media/react-redux.a8b012a0.jpg"
+  },
+  {
+    "revision": "f370c1e0adb80194dd3ea10e468bc69a",
+    "url": "/portfolio/static/media/react.f370c1e0.png"
+  }
+]);
